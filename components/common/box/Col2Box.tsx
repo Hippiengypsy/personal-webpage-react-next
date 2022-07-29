5bb7b7a06a595292a56fab1d3e24975542a3346c
DESKTOP-OOM0DRK\Kimchi_Highlander <hippiengypsy02@gmail.com>
2022-07-29 19:14:07 +0100

    [Refactor] change the file name
    from Grid2Box.tsx to Col2Box.tsx

--- components/common/box/Col2Box.tsx
+++ components/common/box/Col2Box.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import clsx from "clsx";
 
-type Grid2BoxProps = {
+type Col2BoxProps = {
   readonly children?: React.ReactNode;
   readonly className?: React.ReactNode;
 }
 
-const Grid2Box = (props: Grid2BoxProps) => {
+const Col2Box = (props: Col2BoxProps) => {
     const { children, className } = props
   return (
     <div className={clsx('grid grid-cols-1 max-w-6xl my-auto sm:p-6 md:grid-cols-2 lg:p-8 border-2', className)}>
@@ -15,4 +15,4 @@ const Grid2Box = (props: Grid2BoxProps) => {
   )
 }
 
-export default Grid2Box;
+export default Col2Box;
