e16a392d791bda330f40f73e7397b72d499d083a
DESKTOP-OOM0DRK\Kimchi_Highlander <hippiengypsy02@gmail.com>
2022-08-02 16:00:48 +0100

    [Doc] contents change

--- components/developer/DevSkills.tsx
+++ components/developer/DevSkills.tsx
@@ -21,29 +21,53 @@ const DevSkills = (props: Props) => {
             <li>TailwindCSS</li>
             <li>SASS</li>
             <li>GIT</li>
-          </ItemBox>
-          <ItemBox>
+            </ItemBox>
+            <ItemBox>
             <img src='https://www.business2community.com/wp-content/uploads/2015/10/digital-skills2.jpg.jpg' />
             <div>my dev skills with graphic animation</div>
-          </ItemBox>
-        </Col2Box> */}
+            </ItemBox>
+          </Col2Box> */}
           <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
               <h2 className="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">
                 My Skills
               </h2>
               <div className="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
-                <img className='w-auto h-56 hover:text-gray-900 dark:hover:text-white' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/120px-HTML5_logo_and_wordmark.svg.png" alt=""  />
-                <img className='w-auto h-56 hover:text-gray-900 dark:hover:text-white' src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Javascript-shield.svg/397px-Javascript-shield.svg.png?20180912181046' alt="" />
-                <img className='w-auto h-56 hover:text-gray-900 dark:hover:text-white' src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/120px-CSS3_logo_and_wordmark.svg.png" alt="" />
-                <img className='w-auto h-56 hover:text-gray-900 dark:hover:text-white' src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/121px-React-icon.svg.png" alt="" />
-                <img className='w-auto h-56 hover:text-gray-900 dark:hover:text-white' src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/207px-Nextjs-logo.svg.png?20190307203525" alt="" />
-                <img className='w-auto h-56 hover:text-gray-900 dark:hover:text-white' src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/64px-Typescript_logo_2020.svg.png" alt="" />
-                <img className='w-auto h-56 hover:text-gray-900 dark:hover:text-white' src="https://daqxzxzy8xq3u.cloudfront.net/wp-content/uploads/2019/04/21032431/redux-cover-imgage.jpg" alt="" />
-                <img className='w-auto h-56 hover:text-gray-900 dark:hover:text-white' src="https://www.brcline.com/wp-content/uploads/2016/01/bootstrap-logo-300x240.png" alt="" />
-                <img className='w-auto h-56 hover:text-gray-900 dark:hover:text-white' src="https://mlbaavqoemsp.i.optimole.com/RVoz4hs-gPqb-jQk/w:608/h:343/q:90/https://patch.marketing/wp-content/uploads/2021/09/tailwind.png" alt="" />
-                <img className='w-auto h-56 hover:text-gray-900 dark:hover:text-white' src="https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg" alt="" />
-                <img className='w-auto h-56 hover:text-gray-900 dark:hover:text-white' src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1024px-Git-logo.svg.png" alt="" />
-                <img className='w-auto h-56 hover:text-gray-900 dark:hover:text-white' src="https://www.pngfind.com/pngs/m/61-614478_source-tree-logo-png-transparent-png.png" alt="" />
+                <div className='w-auto h-56'>
+                  <img className='w-full h-full hover:text-gray-900 dark:hover:text-white' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1024px-HTML5_logo_and_wordmark.svg.png" alt=""  />
+                </div>
+                <div className='w-auto h-56'>
+                  <img className='w-full h-full hover:text-gray-900 dark:hover:text-white' src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Javascript-shield.svg/397px-Javascript-shield.svg.png?20180912181046' alt="" />
+                </div>
+                <div className='w-auto h-56'>
+                  <img className='w-full h-full hover:text-gray-900 dark:hover:text-white' src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/120px-CSS3_logo_and_wordmark.svg.png" alt="" />
+                </div>
+                <div className='w-auto h-56'>
+                  <img className='w-full h-full hover:text-gray-900 dark:hover:text-white' src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/121px-React-icon.svg.png" alt="" />
+                </div>
+                <div className='w-auto h-56'>
+                  <img className='w-full h-full hover:text-gray-900 dark:hover:text-white' src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/207px-Nextjs-logo.svg.png?20190307203525" alt="" />
+                </div>
+                <div className='w-auto h-56'>
+                  <img className='w-full h-full hover:text-gray-900 dark:hover:text-white' src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/64px-Typescript_logo_2020.svg.png" alt="" />
+                </div>
+                <div className='w-auto h-56'>
+                  <img className='w-full h-full hover:text-gray-900 dark:hover:text-white' src="https://daqxzxzy8xq3u.cloudfront.net/wp-content/uploads/2019/04/21032431/redux-cover-imgage.jpg" alt="" />
+                </div>
+                <div className='w-auto h-56'>
+                  <img className='w-full h-full hover:text-gray-900 dark:hover:text-white' src="https://www.brcline.com/wp-content/uploads/2016/01/bootstrap-logo-300x240.png" alt="" />
+                </div>
+                <div className='w-auto h-56'>
+                  <img className='w-full h-full hover:text-gray-900 dark:hover:text-white' src="https://mlbaavqoemsp.i.optimole.com/RVoz4hs-gPqb-jQk/w:608/h:343/q:90/https://patch.marketing/wp-content/uploads/2021/09/tailwind.png" alt="" />
+                </div>
+                <div className='w-auto h-56'>
+                  <img className='w-full h-full hover:text-gray-900 dark:hover:text-white' src="https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg" alt="" />
+                </div>
+                <div className='w-auto h-56'>
+                  <img className='w-full h-full hover:text-gray-900 dark:hover:text-white' src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/1024px-Git-logo.svg.png" alt="" />
+                </div>
+                <div className='w-auto h-56'>
+                  <img className='w-full h-full hover:text-gray-900 dark:hover:text-white' src="https://www.pngfind.com/pngs/m/61-614478_source-tree-logo-png-transparent-png.png" alt="" />
+                </div>
               </div>
           </div>
       </SectionBox>
--- components/developer/DevStrengths.tsx
+++ components/developer/DevStrengths.tsx
@@ -16,72 +16,114 @@ const DevStrengths = (props: Props) => {
             <div>my strenths with animation</div>
           </ItemBox>
           <ItemBox>
-            <li>Diverse skills and point of view</li>
             <li>Creative / problem solving</li>
-            <li>Focus on user experience</li>
-            <li>Attention to detail and quality of work</li>
-            <li>Responding to a client brief</li>
-            <li>Culture and language</li>
+                <li>Mechanical, technical, logical mind</li>
             <li>Working for myself</li>
-            <li>Matching my goal with the company`&apos;`s goal</li>
-            <li>Inspiring others</li>
+                <li>Matching my goal with the company`&apos;`s goal</li>
             <li>Pacesetting, working hard to find the right solution</li>
-            <li>Understanding the structure of how something is made</li>
-            <li>Mechanical, technical, logical mind</li>
+                <li>Inspiring others</li>
             <li>Wider life experience and reference points</li>
-            <li>Giving feedback and persuading clients about best solutions</li>
+                <li>Culture and language</li>
+                <li>Diverse skills and point of view</li>
+            <li>Focus on user experience</li>
+                <li>Responding to a client brief</li>
+                <li>Giving feedback and persuading clients about best solutions</li>
+            <li>Attention to detail and quality of work</li>
+                <li>Understanding the structure of how something is made</li>
           </ItemBox>
         </Col2Box> */}
-          <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-              <div className="max-w-screen-md mb-8 lg:mb-16">
-                  <h2 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">Designed for business teams like yours</h2>
-                  <p className="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
-              </div>
-              <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-                  <div>
-                      <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                          <svg className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
-                      </div>
-                      <h3 className="mb-2 text-xl font-bold dark:text-white">Marketing</h3>
-                      <p className="text-gray-500 dark:text-gray-400">Plan it, create it, launch it. Collaborate seamlessly with all  the organization and hit your marketing goals every month with our marketing plan.</p>
-                  </div>
-                  <div>
-                      <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                          <svg className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
-                      </div>
-                      <h3 className="mb-2 text-xl font-bold dark:text-white">Legal</h3>
-                      <p className="text-gray-500 dark:text-gray-400">Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.</p>
-                  </div>
-                  <div>
-                      <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                          <svg className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clipRule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>                    
-                      </div>
-                      <h3 className="mb-2 text-xl font-bold dark:text-white">Business Automation</h3>
-                      <p className="text-gray-500 dark:text-gray-400">Auto-assign tasks, send Slack messages, and much more. Now power up with hundreds of new templates to help you get started.</p>
-                  </div>
-                  <div>
-                      <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                          <svg className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"></path><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clipRule="evenodd"></path></svg>
-                      </div>
-                      <h3 className="mb-2 text-xl font-bold dark:text-white">Finance</h3>
-                      <p className="text-gray-500 dark:text-gray-400">Audit-proof software built for critical financial operations like month-end close and quarterly budgeting.</p>
-                  </div>
-                  <div>
-                      <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                          <svg className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path></svg>
-                      </div>
-                      <h3 className="mb-2 text-xl font-bold dark:text-white">Enterprise Design</h3>
-                      <p className="text-gray-500 dark:text-gray-400">Craft beautiful, delightful experiences for both marketing and product with real cross-company collaboration.</p>
-                  </div>
-                  <div>
-                      <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
-                          <svg className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd"></path></svg>
-                      </div>
-                      <h3 className="mb-2 text-xl font-bold dark:text-white">Operations</h3>
-                      <p className="text-gray-500 dark:text-gray-400">Keep your company’s lights on with customizable, iterative, and structured workflows built for all efficient teams and individual.</p>
-                  </div>
-              </div>
-          </div>
+        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <div className="max-w-screen-md mb-8 lg:mb-16">
+                <h2 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">
+                    Designed for business teams like yours
+                </h2>
+                <p className="text-gray-500 sm:text-xl dark:text-gray-400">
+                    Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.
+                </p>
+            </div>
+            <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+                <div>
+                    <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M11 3a1 1 0 10-2 0v1a1 1 0 102 0V3zM15.657 5.757a1 1 0 00-1.414-1.414l-.707.707a1 1 0 001.414 1.414l.707-.707zM18 10a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1zM5.05 6.464A1 1 0 106.464 5.05l-.707-.707a1 1 0 00-1.414 1.414l.707.707zM5 10a1 1 0 01-1 1H3a1 1 0 110-2h1a1 1 0 011 1zM8 16v-1h4v1a2 2 0 11-4 0zM12 14c.015-.34.208-.646.477-.859a4 4 0 10-4.954 0c.27.213.462.519.476.859h4.002z" />
+                        </svg>
+                    </div>
+                    <h3 className="mb-2 text-xl font-bold dark:text-white">
+                        Creative problem solver
+                    </h3>
+                    <p className="text-gray-500 dark:text-gray-400">
+                        Plan it, create it, launch it. Collaborate seamlessly with all  the organization and hit your marketing goals every month with our marketing plan.
+                    </p>
+                </div>
+                <div>
+                    <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clipRule="evenodd" />
+                            <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
+                        </svg>
+                    </div>
+                    <h3 className="mb-2 text-xl font-bold dark:text-white">
+                        Working for myself
+                    </h3>
+                    <p className="text-gray-500 dark:text-gray-400">
+                        Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.
+                    </p>
+                </div>
+                <div>
+                    <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <h3 className="mb-2 text-xl font-bold dark:text-white">
+                        Pace setter
+                    </h3>
+                    <p className="text-gray-500 dark:text-gray-400">
+                        Auto-assign tasks, send Slack messages, and much more. Now power up with hundreds of new templates to help you get started.
+                    </p>
+                </div>
+                <div>
+                    <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </div>
+                    <h3 className="mb-2 text-xl font-bold dark:text-white">
+                        Wider life experience and reference points
+                    </h3>
+                    <p className="text-gray-500 dark:text-gray-400">
+                        Audit-proof software built for critical financial operations like month-end close and quarterly budgeting.
+                    </p>
+                </div>
+                <div>
+                    <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M9 9a2 2 0 114 0 2 2 0 01-4 0z" />
+                            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a4 4 0 00-3.446 6.032l-2.261 2.26a1 1 0 101.414 1.415l2.261-2.261A4 4 0 1011 5z" clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <h3 className="mb-2 text-xl font-bold dark:text-white">
+                        Focus on user experience
+                    </h3>
+                    <p className="text-gray-500 dark:text-gray-400">
+                        Craft beautiful, delightful experiences for both marketing and product with real cross-company collaboration.
+                    </p>
+                </div>
+                <div>
+                    <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-blue-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+                        </svg>
+                    </div>
+                    <h3 className="mb-2 text-xl font-bold dark:text-white">
+                        Attention to detail and quality of work
+                    </h3>
+                    <p className="text-gray-500 dark:text-gray-400">
+                        Keep your company’s lights on with customizable, iterative, and structured workflows built for all efficient teams and individual.
+                    </p>
+                </div>
+            </div>
+        </div>
       </SectionBox>
     </Element>
   )
