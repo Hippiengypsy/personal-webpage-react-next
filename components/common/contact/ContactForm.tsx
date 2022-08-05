f23750dd9cd4fe0bbba8b97482d0f5dee99b9155
DESKTOP-OOM0DRK\Kimchi_Highlander <hippiengypsy02@gmail.com>
2022-08-05 16:40:37 +0100

    [Feat] apply contact template

--- components/common/contact/ContactForm.tsx
+++ components/common/contact/ContactForm.tsx
@@ -4,26 +4,29 @@ type Props = {}
 
 const ContactForm = (props: Props) => {
   return (
-    <form className="flex flex-col justify-center">
-        <div className="flex flex-col my-1">
-            <label htmlFor="name" className="hidden">Full Name</label>
-            <input type="name" name="name" id="name" placeholder="Full Name" className="w-100 my-1 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-400 font-semibold focus:border-indigo-500 focus:outline-none" />
+    <div className="">
+        <div>
+            <span className="uppercase text-sm text-gray-600 font-bold">Full Name</span>
+            <input className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="text" placeholder="" />
         </div>
-
-        <div className="flex flex-col my-1">
-            <label htmlFor="email" className="hidden">Email</label>
-            <input type="email" name="email" id="email" placeholder="Email" className="w-100 my-1 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-400 font-semibold focus:border-indigo-500 focus:outline-none" />
+        <div className="mt-8">
+            <span className="uppercase text-sm text-gray-600 font-bold">Email</span>
+            <input className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="text" />
         </div>
-
-        <div className="flex flex-col my-1">
-            <label htmlFor="tel" className="hidden">Number</label>
-            <input type="tel" name="tel" id="tel" placeholder="Telephone Number" className="w-100 my-1 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-400 font-semibold focus:border-indigo-500 focus:outline-none" />
+        <div className="mt-8">
+            <span className="uppercase text-sm text-gray-600 font-bold">Message</span>
+            <textarea
+                className="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
         </div>
-
-        <button type="submit" className="md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg my-3 hover:bg-indigo-500 transition ease-in-out duration-300">
-            Submit
-        </button>
-    </form>
+        <div className="mt-8">
+            <button
+                className="uppercase text-sm font-bold tracking-wide bg-indigo-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline">
+                Send Message
+            </button>
+        </div>
+  </div>
   )
 }
 
