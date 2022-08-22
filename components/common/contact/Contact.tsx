c37e7e423f6f6b94eead7edbd87d86a7cdc62038
Kimchi-Haggis <kimchihaggis@gmail.com>
2022-08-22 15:41:22 +0100

    [Docs] contact section

--- components/common/contact/Contact.tsx
+++ components/common/contact/Contact.tsx
@@ -15,9 +15,21 @@ const Contact = (props: Props) => {
                 <ItemBox boxType='box-item-head'>
                     Contact
                 </ItemBox>
-                <div className="box-border my-20 md:my-20 lg:my-20 ">
-                    <ContactForm />
-                </div>
+                <ItemBox boxType='box-item-col2'>
+                    <div className="box-border my-20 md:my-20 lg:my-20">
+                        <ContactForm />
+                    </div>
+                    <div className='space-y-8 p-4 w-full'>
+                        <div className='space-y-8 p-4 w-full my-20 md:my-20 lg:my-20 text-xl'>
+                            <div>If you want to connect or have any questions, please feel free to get in touch.</div><br />
+                            <div>I'll be happy to grab a coffee and chat.</div>
+                            <div>I have more stories that I could't put here</div>
+                            <div>and also want to hear about your stories as well.</div><br />
+                            <div>Email: kimchihaggis@gmail.com</div><br />
+                            <div>[social media / github links]</div>
+                        </div>
+                    </div>
+                </ItemBox>
             </ContentsBox>
         </SectionBox>
     </Element>
