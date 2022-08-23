50be04cfa49f24164ac43f0ec93bd63aab1a2c65
Kimchi-Haggis <kimchihaggis@gmail.com>
2022-08-23 23:29:02 +0100

    [Feat] scroll to contact when click text

--- components/developer/DevAboutMe.tsx
+++ components/developer/DevAboutMe.tsx
@@ -2,7 +2,7 @@ import ContentsBox from '@components/common/box/ContentsBox'
 import ItemBox from '@components/common/box/ItemBox'
 import SectionBox from '@components/common/box/SectionBox'
 import React from 'react'
-import { Element } from 'react-scroll'
+import { Element, Link } from 'react-scroll'
 
 type Props = {}
 
@@ -15,18 +15,23 @@ const DevAboutMe = (props: Props) => {
                     About Me
                 </ItemBox>
                 <ItemBox boxType='box-item-col2'>
-                    <div>something image</div>
-                    <div className='text-xl'>
+                    <div className='space-y-8 p-4 w-full'>
+                        <img className='opacity-90' src="/img/me.jpg" alt="dreams" />
+                    </div>
+                    <div className='space-y-8 p-4 w-full text-xl'>
                         <div>I'm a front end web developer, born and raised in South Korea and now based in Scotland.</div><br />
 
                         <div>My path to becoming a developer started in 2021 when I worked with Korean tech start-up [Tim and Jim Co].</div>
                         <div>Working 12 hours a day, 6 days a week, my colleagues and I learnt the programming skills we needed to launch the company's first service, a new type of SNS called Dongle Space.</div>
-                        <div>[Maybe a sentence here to specify what elements you worked on].</div><br />
-
                         <div>It was hard but I found a lot of joy in solving problems, working with razor sharp focus and applying my skills in the real world.</div>
-                        <div>Since moving to Scotland, I have been studying programming languages and building projects.</div><br />
-                        
-                        <div>I am currently looking to join a passionate and dynamic team. If you have a job role open, please get in touch! [link]</div>
+                        <div>That was my "Zero to One" moment of my coding experience.</div><br />
+
+                        <div>Since moving to Scotland, I have been carrying on improving my skills and building projects.</div><br />
+
+                        <div>I am currently looking to join a passionate and dynamic team. If you have a job role open,</div>
+                        <div className='underline'>
+                            <Link  to="contact" smooth={true} duration={500}>please get in touch!</Link>
+                        </div>
                         <div> I enjoy talking to different people and speak good conversational English - and even a wee bit of Scottish, ken. It would be great to hear from you.</div>
                     </div>
                 </ItemBox>
