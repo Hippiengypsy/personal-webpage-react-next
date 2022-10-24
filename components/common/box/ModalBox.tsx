f35652186ff3e542a5dc60da9d9d9d788d0444d0
Kimchi-Haggis <kimchihaggis@gmail.com>
2022-10-24 15:38:18 +0100

    [Fix] fixed close modal button

--- components/common/box/ModalBox.tsx
+++ components/common/box/ModalBox.tsx
@@ -22,9 +22,9 @@ const ModalBox = (props: ModalBoxProps) => {
                             <button
                                 type="button"
                                 onClick={handleClose}
-                                className="p-1.5 float-right"
+                                className="p-4 sm:p-6 fixed object-right"
                             >
-                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                <svg aria-hidden="true" className="w-5 h-5 lg:w-8 lg:h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                             </button>
                             <div className="">
                                 {children}
